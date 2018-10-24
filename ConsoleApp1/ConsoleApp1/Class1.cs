4ad9fa1df40968971cabe4a7d89ce21f4e00fbc4
Leonardo Marques <sandstorm603@gmail.com>
2018-10-23 23:40:40 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class ContaPoupanca
+    {
+        static  readonly  double public TaxaDeJurosAnual {
+            double Taxa = 0.4;
+        }//Fim TaxaAnual
+    }//Fim ContaPoupanca
+}//Fim Namespace
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public double  Poupador1() {
+
+          private double saldoPoupanca = 3000.00;
+          private double CalcularJuroMensal = saldoPoupanca * TaxaDeJurosAnual / 12;
+
+          return saldoPoupanca - CalcularJuroMensal;
+        }//Fim poupador1
+
+       public double Poupador2() {
+
+          private double SaldoPoupanca = 3000.00;
+          private double CalcularJuroMensal = SaldoPoupanca * TaxaDeJurosAnual / 12;
+         
+          return SaldoPoupanca - CalcularJuroMensal;
+        }//Fim poupador2
+    }
+}
